ddb3c60e34ec28cd4dfe4a87db407e5ec1ebdb5d
Geoff Humphreys <humphreys.geoff@gmail.com>
2017-06-26 15:19:54 -0400

    the bulk of this code is not mine.

---
+++ userData.js
@@ -0,0 +1,16 @@
+module.exports = {
+  users: [
+    {
+      username: "sup",
+      password: "dog"
+    },
+    {
+      username: "grayson",
+      password: "dinotrux"
+    },
+    {
+      username: "lilamae",
+      password: "aye-aye"
+    }
+  ]
+}
